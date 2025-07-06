fb45aa198c6ad38e9c1d647cb44a90ce9a694242
ShubhamPuri-SDET <Spuri4867@gmail.com>
2025-07-06 18:58:01 +0530

    Added class for naukari profile resume update

---
+++ src/test/java/naukri_profile_automation/UpdateResume.java
@@ -0,0 +1,83 @@
+package naukri_profile_automation;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class UpdateResume {
+
+	public static void main(String[] args) throws InterruptedException, URISyntaxException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.naukri.com/");
+		driver.manage().window().maximize();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		try {
+
+			WebElement gotItButton = wait.until(ExpectedConditions.elementToBeClickable(
+					By.xpath("//div[@class='acceptance-btn']//span[normalize-space()='Got it']")));
+
+			// Scroll into view and click using JavaScript
+			js.executeScript("arguments[0].scrollIntoView({block: 'center'});", gotItButton);
+			js.executeScript("arguments[0].click();", gotItButton);
+
+			System.out.println("Cookie banner dismissed.");
+
+		} catch (TimeoutException e) {
+			System.out.println("Cookie banner not found or already dismissed.");
+		}
+
+		driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
+
+		WebElement emailInput = wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//input[@type='text' and contains(@placeholder, 'Enter your active Email ID / Username')]")));
+		emailInput.sendKeys("spuri4867@gmail.com");
+
+// driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']")).sendKeys("spuri4867@gmail.com");
+		driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("Shubham@29051");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='View profile']")))
+				.click();
+
+		WebElement updateResume = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//input[@type='button' and @value='Update resume']")));
+
+		js.executeScript("arguments[0].scrollIntoView({block: 'center'});", updateResume);
+		js.executeScript("arguments[0].click();", updateResume);
+
+		Thread.sleep(1000);
+
+		WebElement fileInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("attachCV")));
+
+		URL resourceUrl = UpdateResume.class.getClassLoader()
+				.getResource("Shubham_Puri_TestAutomationEngineer_Resume.pdf");
+
+		if (resourceUrl == null) {
+			throw new RuntimeException("Resume file not found in test resources.");
+		}
+
+		File resumeFile = new File(resourceUrl.toURI());
+		fileInput.sendKeys(resumeFile.getAbsolutePath());
+
+		// String filePath = System.getProperty("user.home") +
+		// "\\Desktop\\Shubham_Puri_TestAutomationEngineer_Resume.pdf";
+		// fileInput.sendKeys(filePath);
+		System.out.println("Clicked on 'Update resume' and uploaded the file.");
+
+		driver.quit();
+
+	}
+}
